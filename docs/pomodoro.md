ecd9bbf119bea9110f6dd41c4b6e90c944f7c2cb
sonicakes <soniaparker1207@gmail.com>
2018-08-25 19:51:30 +1000

    tests for Customers and Tours Controllers

--- docs/pomodoro.md
+++ docs/pomodoro.md
@@ -6,5 +6,8 @@ Started writing routes
 # Sat 25 aug , 6.25 pm
 start writing test with Controllers using GEM "rails-testing controllers"
 
-#Sat 25 aug, 7.15 pm
+# Sat 25 aug, 7.15 pm
+Wrote test for Customer and Tour Controllers
+
+# Sun 26 Aug, 9.30 am
 
--- taste_of_russia/spec/controllers/tours_controller_spec.rb
+++ taste_of_russia/spec/controllers/tours_controller_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe ToursController, type: :controller do
   describe "GET index" do
-    it 'should pass a @tours object to the view'
+
+    before do
+      get :index
+    end
+
+    it 'should pass a @tours object to the view' do
+          expect(assigns(:tours)).to be
+    end
   end
 
   describe "GET show" do
-    it 'should pass a @tour object to the view'
+
+    before do
+      tour = Tour.create(
+        :title => "St Petersburg to Moscow",
+        :description => "Foo",
+        :price => 500
+      )
+      get :show, :params => { :id => tour.id }
+    end
+
+    it 'should pass a @tour object to the view' do
+      expect(assigns(:tour)).to be
+    end
+
   end
 end
