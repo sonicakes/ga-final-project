7c30ea295fe04e980da88d9e572ad590e47dd33f
sonicakes <soniaparker1207@gmail.com>
2018-08-26 11:50:24 +1000

    finished Controller tests

--- taste_of_russia/app/controllers/bookings_controller.rb
+++ taste_of_russia/app/controllers/bookings_controller.rb
@@ -1,2 +1,67 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+    @tour = Tour.find params[:tour_id]
+   end
+
+   def show
+    @booking = Booking.find params[:id]
+   end
+
+ def create
+
+     # Every booking needs to belong to a customer
+     # If the customer already has an account and is logged in, then we use
+     # that customer object.
+
+     # If they are not logged in, then we need to try making a new customer
+     # record using the details they entered into the form.
+
+     # The saving of the new customer, could fail (e.g. they didn't enter a
+     # password, the email address is already in use, etc.)
+
+     # If we can't save the new customer, then we redirect back to the booking
+     # new page, immediately.
+
+     customer = Customer.new
+     customer.first_name = params[:customer_first_name]
+     customer.last_name = params[:customer_last_name]
+     customer.email = params[:customer_email]
+     customer.password = params[:customer_password]
+     customer.phone_number = params[:customer_phone_number]
+
+     unless customer.save
+        redirect_to new_booking_url and return
+     end
+
+     #is the customer logged in?
+     #if not, make a customer object using details they send in a form
+     # if it fails,
+    #make a booking object
+
+
+    #assign all the variables
+    # try and save it
+    #if saving successful, redirect tobooking#show
+    #if not, redirect booking#new form again
+   booking = Booking.new
+   booking.start_date = params[:start_date]
+   booking.tour_id = params[:tour_id]
+   booking.customer_id = customer.id
+
+   unless booking.save
+      redirect_to new_booking_url and return
+   end
+  #we now saved the booking, we now need to add Booking People to the booking.
+
+
+   redirect_to booking
+
+ end
+
+
 end
--- taste_of_russia/spec/controllers/bookings_controller_spec.rb
+++ taste_of_russia/spec/controllers/bookings_controller_spec.rb
@@ -3,32 +3,155 @@ require 'rails_helper'
 RSpec.describe BookingsController, type: :controller do
 
   describe "GET index" do
-    it "should pass @bookings object to the view"
+    before do
+      get :index
+    end
+
+    it "should pass @bookings object to the view" do
+        expect(assigns(:bookings)).to be
+    end
   end
 
   describe "GET new" do
-    it 'should pass a @tour object to the view'
-    # if the customer is logged in, this should be an object
+    before do
+        tour = Tour.create(
+          :title => "St Petersburg to Moscow",
+          :description => "Foo",
+          :price => 500
+        )
+      get :new, :params => { :tour_id => tour.id}
+    end
+
+    it 'should pass a @tour object to the view' do
+      expect(assigns(:tour)).to be
+    end
+    # TODO:if the customer is logged in, this should be an object
     # if its a brand new customer, it should be nil
-    it 'should pass a @customer variable to the view'
+
+    # it 'should pass a @customer variable to the view' do
+    #     expect(assigns(:customer)).to be
+    # end
   end
 
 describe "GET show" do
-  it 'should pass @booking object to the view'
+
+# TODO create a booking, customer, tour object for this to work
+
+    before do
+
+
+        tour = Tour.create(
+          :title => "St Petersburg to Moscow",
+          :description => "Foo",
+          :price => 500
+        )
+
+
+        customer = Customer.create(
+          :password => "bob12345",
+          :first_name => "Robert",
+          :email => "bob-smith@example.com",
+          :last_name => "Smith",
+          :phone_number => "12-34-56"
+        )
+
+        booking = Booking.create(
+          :payment_reference => "12345",
+          :start_date => "2018-09-24",
+          :tour_id => tour.id,
+          :customer_id => customer.id
+        )
+
+      get :show, :params => { :id => booking.id}
+    end
+
+  it 'should pass @booking object to the view' do
+    expect(assigns(:booking)).to be
+  end
 end
 
   describe "POST create" do
-    describe 'A booking with correct information and successful payment' do
-      it 'should redirect to booking#show page'
+
+    describe 'A booking with a new customer and successful payment' do
+
+      before do
+
+        tour = Tour.create(
+          :title => "St Petersburg to Moscow",
+          :description => "Foo",
+          :price => 500
+        )
+
+        post :create, :params => {
+          :tour_id => tour,
+          :start_date => '2018-09-24',
+          :customer_first_name => 'Rowik',
+          :customer_last_name =>'Boikov',
+          :customer_phone_number => "123456",
+          :customer_email => "test@example.com",
+          :customer_password => "chicken"
+        }
+
+      end
+
+      it 'should redirect to booking#show page' do
+        expect(response).to redirect_to Booking.last
+      end
       # TODO it should show a success message - thanks for booking
     end
-    describe 'A booking with correct information, but payment fails' do
-      it 'should redirect to booking#show'
+
+    describe 'A booking with a new customer, but payment fails' do
+
+      before do
+
+        tour = Tour.create(
+          :title => "St Petersburg to Moscow",
+          :description => "Foo",
+          :price => 500
+        )
+
+        post :create, :params => {
+          :tour_id => tour,
+          :start_date => '2018-09-24',
+          :customer_first_name => "Rowik",
+          :customer_last_name =>'Boikov',
+          :customer_phone_number => "123456",
+          :customer_email => "test@example.com",
+          :customer_password => "chicken"
+        }
+      end
+
+      it 'should redirect to booking#show' do
+        expect(response).to redirect_to Booking.last
+      end
       # TODO the page should prompt them to try payment again
       # thanks for booking, but we couldn't process your payment...
     end
-    describe 'A booking with incorrect information' do
-      it 'should redirect to booking#new'
+
+    describe 'A booking with a new user, but customer fields are empty' do
+
+      before do
+
+        tour = Tour.create(
+          :title => "St Petersburg to Moscow",
+          :description => "Foo",
+          :price => 500
+        )
+
+        post :create, :params => {
+          :tour_id => tour,
+          :start_date => '2018-09-24',
+          :customer_first_name => '',
+          :customer_last_name =>'',
+          :customer_phone_number => '',
+          :customer_email => '',
+          :customer_password => ''
+        }
+      end
+
+      it 'should redirect to booking#new' do
+        expect(response).to redirect_to :action => :new
+      end
       # TODO they should be shown an error message
     end
   end
